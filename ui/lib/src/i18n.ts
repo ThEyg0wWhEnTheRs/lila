4ee1d50882ddf47d57f65022d62aab4c7c22c4db
Simek <gosimek@gmail.com>
2026-03-01 10:14:38 +0100

    small simplification

--- ui/lib/src/i18n.ts
+++ ui/lib/src/i18n.ts
@@ -20,7 +20,7 @@ export const timeago: (d: DateLike) => string = (date: DateLike) =>
 // format Date / string / timestamp to Date instance.
 export const toDate = (input: DateLike): Date => {
   if (input instanceof Date) return input;
-  else return new Date(input);
+  return new Date(input);
 };
 
 export const use24h = (): boolean => !commonDateFormatter.resolvedOptions().hour12;
